5733dd8418a6f7fffc2c235efeb4fe39b6589bf9
Vishesh Raj <visheshrajput47@gmail.com>
2025-06-23 00:55:08 +0530

    new commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from "./Context_api_Project/App.jsx"
+import App from "./Practise/App.jsx"
 import './index.css';
 
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}", // adjust based on your setup
   ],
+  darkMode: "class",
   theme: {
     extend: {},
   },
